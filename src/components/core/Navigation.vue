ef35776ed078dcc69db6dbc81015cd9a60976064
Prince Goziem <princegoziem@gmail.com>
2019-05-14 15:33:33 +0100

    top navigation ui implemented

--- src/components/core/Navigation.vue
+++ src/components/core/Navigation.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="dt ph3-ns top-nav w-100 tl border-box fw6 h3-ns">
-    <div class="v-mid dtc w-10">
-      <a href="/">Logo</a>
+  <div
+    class="flex ph3-ns items-center top-nav w-100 tl border-box f4 lh-title fw6 h3-ns bb b--light-gray"
+  >
+    <div class="w-20 flex items-center h-100">
+      <img class="pr3" src="../../assets/icons/burger-menu.svg" alt="Burger Menu" width="25">
+      <a class="logo no-underline dark-gray fw9" href="/">BUY-LESS</a>
     </div>
-    <div class="v-mid dtc">Hello</div>
-    <div class="v-mid dtc w-20 tr">Hi</div>
+    <div class="w-100 bl pl3 b--light-gray h-100 flex items-center">
+      <img class src="../../assets/icons/search.svg" alt="Search icon" width="25">
+    </div>
+    <div class="w-60 tr top-link h-100">
+      <a class="no-underline silver bl b--light-gray" href="#0">Sign In</a>
+      <a class="no-underline silver bl b--light-gray" href="#0">Sign Up</a>
+    </div>
   </div>
 </template>
 
@@ -15,8 +23,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.top-nav {
-  /* background-color: blue; */
-  /* border: 1px solid #000; */
+.logo {
+  letter-spacing: 0.2rem;
+}
+.top-link .no-underline {
+    display: inline-block;
+    padding: 1.2rem 3rem;
 }
 </style>
